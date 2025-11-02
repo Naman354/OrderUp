138eefb0856ee4725fdd86939ebda46125d6a1a6
Chinmoy Senapoti <chinmoysenapoti77x@gmail.com>
2025-11-03 02:12:29 +0530

    done the menu feature - data and domain layer

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
