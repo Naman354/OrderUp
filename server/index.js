eea4cb3f9d94ec1e3b4f97b99fb40fc7bacb9b44
Naman354 <srinaman05@gmail.com>
2025-11-06 02:08:32 +0530

    Added tested Socket.io connections for the backend

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log("⚡ New client connected:", socket.id);
-
+   console.log("Handshake query:", socket.handshake);  
   socket.on("join", (role, userId) => {
     if (role === "admin") socket.join("admins");
     else if (role === "student") socket.join(userId);
